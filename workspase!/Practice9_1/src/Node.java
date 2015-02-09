b8cd7defcfa32a8d6f5515ab7e7e4183482c1f0e
riznyk7 <riznyk7@gmail.com>
2015-02-09 13:34:11 +0200

    FirstCommit

---
+++ workspase!/Practice9_1/src/Node.java
@@ -0,0 +1,13 @@
+
+public class Node {
+	private Node[] nodes;
+
+	public Node() {
+		this.nodes = new Node[10];
+	}
+	
+	public void add(){
+		nodes[] = node;
+	}
+
+}
---
+++ workspase!/Practice9_1/src/Number.java
@@ -0,0 +1,18 @@
+
+public class Number {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+	
+	
+	
+	public Number(){
+		
+	}
+
+}
